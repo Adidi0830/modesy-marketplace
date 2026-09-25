d50656b73cb4aa35228be9c4e98ee33c7071a469
Adidi0830 <muhammadadialsaadan@gmail.com>
2026-09-25 14:27:45 +0700

    v8 fix snap url sandbox

--- src/components/checkout/CheckoutClient.tsx
+++ src/components/checkout/CheckoutClient.tsx
@@ -339,7 +339,9 @@ export function CheckoutClient() {
       <Script
         src={
           process.env.NEXT_PUBLIC_MIDTRANS_SNAP_URL ||
-          "https://app.sandbox.midtrans.com/snap/snap.js"
+          (process.env.NEXT_PUBLIC_MIDTRANS_IS_PRODUCTION === "true"
+            ? "https://app.midtrans.com/snap/snap.js"
+            : "https://app.sandbox.midtrans.com/snap/snap.js")
         }
         data-client-key={
           process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY ||
